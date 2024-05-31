3768109d73c3d1c94ee769a094192221c597d5ee
Irmansyah Muslimin <117552270+kiraascoder@users.noreply.github.com>
2024-05-31 21:06:57 +0800

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
-import router from "./routes/route.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
-app.use("/", router);
 dotenv.config(); // Load the environment variables
 
 const url = process.env.DATABASE_URL;
@@ -18,13 +16,9 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log("Connected to MongoDB");
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log(err));
 
-    app.listen(port, () => {
-      console.log("Server running on port " + port);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
