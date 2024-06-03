1467373c0984f2e06aac906100bba5bde1cdc124
Irmansyah Muslimin <117552270+kiraascoder@users.noreply.github.com>
2024-06-03 19:27:26 +0800

    fix/test prisma schema

--- server/index.js
+++ server/index.js
@@ -2,16 +2,30 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
-const app = express();
-
-
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config(); // Load the environment variables
+const app = express();
 const url = process.env.DATABASE_URL;
 const port = 3000;
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors({ origin: true }));
+
+async function main() {
+ const user = await prisma.user.create({
+    data: {
+      name: "Irmansyah Muslimin",
+      email: "Gy5uX@example.com",
+      password: "12345",
+      phoneNumber: "081234567890",
+    },
+  });
+  console.log(user); // ... you will write your Prisma Client queries here
+}
+
+main();
 
 mongoose
   .connect(url, {
