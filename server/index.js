d1dc5ecf8c9ee29ca2f77674ecd0f7b5469ad7cd
Irmansyah Muslimin <117552270+kiraascoder@users.noreply.github.com>
2024-06-03 17:56:09 +0800

    Update Prisma

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
-
 const app = express();
-app.use(express.json());
-app.use(cors({ origin: true }));
-dotenv.config(); // Load the environment variables
 
+
+
+dotenv.config(); // Load the environment variables
 const url = process.env.DATABASE_URL;
 const port = 3000;
+
+app.use(express.json());
+app.use(cors({ origin: true }));
 
 mongoose
   .connect(url, {
