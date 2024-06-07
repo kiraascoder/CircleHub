c6f467addc3ac8070f783395a52cb69e7e244e70
Irmansyah Muslimin <117552270+kiraascoder@users.noreply.github.com>
2024-06-07 22:45:28 +0800

    add auth middleware

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Register User
 export const register = async (req, res) => {
@@ -33,16 +34,36 @@ export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
   if (user) {
     const isMatch = await bcrypt.compare(req.body.password, user.password);
+    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
     if (isMatch) {
       return res.status(200).json({ success: true, message: "Login success" });
     } else {
       return res
         .status(400)
-        .json({ success: false, message: "Wrong password" });
+        .json({ token, success: false, message: "Wrong password" });
     }
   } else {
     return res
       .status(400)
       .json({ success: false, message: "User does not exist" });
+  }
+};
+
+// Logout
+
+export const logout = async (req, res) => {
+  return res.status(200).json({ success: true, message: "Logout success" });
+};
+
+// Get User
+
+export const getUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    return res.status(200).json({ user });
+  } else {
+    return res.status(404).json({ message: "User not found" });
   }
 };
