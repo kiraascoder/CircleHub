1ad4baa4c1efc4e3b3c2f33515d3aab9574dec33
Irmansyah Muslimin <117552270+kiraascoder@users.noreply.github.com>
2024-07-24 13:28:25 +0800

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -317,31 +317,3 @@ export const changeAvatar = async (req, res, next) => {
     }
   });
 };
-export const followers = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return next(new HttpError("User not found", 404));
-    }
-    const followers = await User.find({ _id: { $in: user.followers } }).select(
-      "-password"
-    );
-    res.status(200).json({ success: true, followers });
-  } catch (error) {
-    return next(new HttpError(error.message, 500));
-  }
-};
-export const followings = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return next(new HttpError("User not found", 404));
-    }
-    const followings = await User.find({
-      _id: { $in: user.followings },
-    }).select("-password");
-    res.status(200).json({ success: true, followings });
-  } catch (error) {
-    return next(new HttpError(error.message, 500));
-  }
-};
